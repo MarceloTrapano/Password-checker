17e5ae8c7f646c703b0b9808857cf99e2e3fc8f1
MarceloTrapano <kacper7866@gmail.com>
2026-07-04 01:07:23 +0200

    Add docstrings to main file

--- main.py
+++ main.py
@@ -4,24 +4,37 @@ import os
 from src import PasswordModel, PasswordResponseModel, password_check, prepare_user_inputs
 
 
-LANGUAGE_PATH = 'lang-english.txt'
+LANGUAGE_PATH: str = 'lang-english.txt'
 with open(os.path.abspath(LANGUAGE_PATH), "r") as f:
-    GLOBAL_WORDS = f.read().splitlines()
+    GLOBAL_WORDS: list[str] = f.read().splitlines()
 
-app = FastAPI()
+app: FastAPI = FastAPI()
 
 
 @app.get("/")
-def home():
+def home() -> dict[str, str]:
+    """Root API response.
+
+    Returns:
+        dict[str, str]: welcoming message.
+    """
     return {"message": "Hello there! This is password checker! Please send your password check via our API!"}
 
 
 @app.post("/check_password/", response_model=PasswordResponseModel)
-def post_password(package: PasswordModel):
-    user_inputs = prepare_user_inputs(
+def post_password(package: PasswordModel) -> dict[str, str | int | bool | list[str]]:
+    """Validate user password.
+
+    Args:
+        package (PasswordModel): password package with credentials such as username, email and optional previous password.
+
+    Returns:
+        dict[str, str | int | bool | list[str]]: password strength evaluation.
+    """
+    user_inputs: list[str] = prepare_user_inputs(
         username=package.username, email=package.email)
 
     user_inputs.extend(GLOBAL_WORDS)
-    response = password_check(
+    response: dict[str, str | int | bool | list[str]] = password_check(
         password=package.password, user_inputs=user_inputs, prev_password=package.prev_password)
     return response
