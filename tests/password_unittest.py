4e11656286d89c557d74e845068e0f52bd624960
MarceloTrapano <kacper7866@gmail.com>
2026-07-04 01:06:23 +0200

    Clean up unit tests.

--- tests/password_unittest.py
+++ tests/password_unittest.py
@@ -2,139 +2,131 @@ import unittest
 import os
 from src import password_check, prepare_user_inputs
 
-LANGUAGE_PATH = 'lang-english.txt'
+LANGUAGE_PATH: str = 'lang-english.txt'
 with open(os.path.abspath(LANGUAGE_PATH), "r") as f:
-    GLOBAL_WORDS = f.read().splitlines()
+    GLOBAL_WORDS: list[str] = f.read().splitlines()
 
 
 class TestPasswordChecker(unittest.TestCase):
+    """Password testing environment."""
 
-    def test_easy(self):
-        password = "MamaTata1"
-        username = "jkowalski"
-        email = "jan.kowalski@securebank.pl"
-
-        user_inputs = prepare_user_inputs(username=username, email=email)
+    def _check(self, password: str, username: str = "jkowalski",
+               email: str = "jan.kowalski@securebank.pl", prev_password: str = None) -> dict[str, str | int | bool | list[str]]:
+        """Helper to prepare inputs and run the checker."""
+        user_inputs: list[str] = prepare_user_inputs(
+            username=username, email=email)
         user_inputs.extend(GLOBAL_WORDS)
-        result = password_check(password=password, user_inputs=user_inputs)
+        return password_check(password=password, user_inputs=user_inputs, prev_password=prev_password)
+
+    def test_easy(self) -> None:
+        """Simple password test."""
+        password: str = "MamaTata1"
+
+        result: dict[str, str | int | bool | list[str]
+                     ] = self._check(password=password)
 
         self.assertFalse(result["is_compliant"])
 
-    def test_easy_password(self):
-        password = "qwerty123456789"
-        username = "jkowalski"
-        email = "jan.kowalski@wp.pl"
+    def test_easy_password(self) -> None:
+        """Easy long password test."""
+        password: str = "qwerty123456789"
 
-        user_inputs = prepare_user_inputs(username=username, email=email)
-        user_inputs.extend(GLOBAL_WORDS)
-        result = password_check(password=password, user_inputs=user_inputs)
+        result: dict[str, str | int | bool | list[str]
+                     ] = self._check(password=password)
 
         self.assertFalse(result["is_compliant"])
 
-    def test_hard_password_connected(self):
-        password = "janek2026securebank!?"
-        username = "jkowalski"
-        email = "jan.kowalski@securebank.pl"
+    def test_hard_password_connected(self) -> None:
+        """Long and hard password but with hints in username and email adress."""
+        password: str = "janek2026securebank!?"
+        username: str = "jkowalski"
+        email: str = "jan.kowalski@securebank.pl"
 
-        user_inputs = prepare_user_inputs(username=username, email=email)
-        user_inputs.extend(GLOBAL_WORDS)
-        result = password_check(password=password, user_inputs=user_inputs)
-
-        self.assertFalse(result["is_compliant"])
-
-    def test_hard_common_words(self):
-        password = "ilovemywifekasia!"
-        username = "jkowalski"
-        email = "jan.kowalski@securebank.pl"
-
-        user_inputs = prepare_user_inputs(username=username, email=email)
-        user_inputs.extend(GLOBAL_WORDS)
-        result = password_check(password=password, user_inputs=user_inputs)
+        result: dict[str, str | int | bool | list[str]] = self._check(
+            password=password, username=username, email=email)
 
         self.assertFalse(result["is_compliant"])
 
-    def test_hard_eset_password(self):
-        password = "+$D@=0~<LPG@d@o"
-        username = "jkowalski"
-        email = "jan.kowalski@securebank.pl"
+    def test_hard_common_words(self) -> None:
+        """Long and hard password but with common words."""
+        password: str = "ilovemywifekasia!"
 
-        user_inputs = prepare_user_inputs(username=username, email=email)
-        user_inputs.extend(GLOBAL_WORDS)
-        result = password_check(password=password, user_inputs=user_inputs)
+        result: dict[str, str | int | bool | list[str]
+                     ] = self._check(password=password)
+
+        self.assertFalse(result["is_compliant"])
+
+    def test_hard_eset_password(self) -> None:
+        """Hard and long password generated via eset."""
+        password: str = "+$D@=0~<LPG@d@o"
+
+        result: dict[str, str | int | bool | list[str]
+                     ] = self._check(password=password)
 
         self.assertTrue(result["is_compliant"])
         self.assertEqual(result["score"], 4)
 
-    def test_medium_eset_password(self):
-        password = "G,azBR0p.}.z"
-        username = "jkowalski"
-        email = "jan.kowalski@securebank.pl"
+    def test_medium_eset_password(self) -> None:
+        """Hard but shorter password generated via eset."""
+        password: str = "G,azBR0p.}.z"
 
-        user_inputs = prepare_user_inputs(username=username, email=email)
-        user_inputs.extend(GLOBAL_WORDS)
-        result = password_check(password=password, user_inputs=user_inputs)
+        result: dict[str, str | int | bool | list[str]
+                     ] = self._check(password=password)
 
         self.assertTrue(result["is_compliant"])
         self.assertEqual(result["score"], 3)
 
-    def test_before_prev(self):
-        password = "P@r@mp@sk1l@m2026"
-        username = "jkowalski"
-        email = "jan.kowalski@securebank.pl"
+    def test_before_prev(self) -> None:
+        """Test for checking password without any relation."""
+        password: str = "P@r@mp@sk1l@m2026"
 
-        user_inputs = prepare_user_inputs(username=username, email=email)
-        user_inputs.extend(GLOBAL_WORDS)
-        result = password_check(password=password, user_inputs=user_inputs)
+        result: dict[str, str | int | bool | list[str]
+                     ] = self._check(password=password)
 
         self.assertTrue(result["is_compliant"])
         self.assertEqual(result["score"], 4)
 
-    def test_with_prev(self):
-        prev_password = "P@r@mp@sk1l@m2026"
-        password = "P@r@mp@sk1l@m2077"
-        username = "jkowalski"
-        email = "jan.kowalski@securebank.pl"
+    def test_with_prev(self) -> None:
+        """Test for checking password with relation to previous one."""
+        prev_password: str = "P@r@mp@sk1l@m2026"
+        password: str = "P@r@mp@sk1l@m2077"
 
-        user_inputs = prepare_user_inputs(username=username, email=email)
-        user_inputs.extend(GLOBAL_WORDS)
-        result = password_check(
-            password=password, user_inputs=user_inputs, prev_password=prev_password)
+        result: dict[str, str | int | bool | list[str]] = self._check(
+            password=password, prev_password=prev_password)
 
         self.assertFalse(result["is_compliant"])
 
-    def test_edge_case_empty_or_very_short(self):
-        user_inputs = prepare_user_inputs(
-            username="jkowalski", email="jan@wp.pl")
-        user_inputs.extend(GLOBAL_WORDS)
+    def test_edge_case_empty_or_very_short(self) -> None:
+        """Verify that short passwords are rejected."""
 
-        result_empty = password_check(password="", user_inputs=user_inputs)
-        result_short = password_check(password="A1!", user_inputs=user_inputs)
+        result_empty: dict[str, str | int | bool |
+                           list[str]] = self._check(password="")
+        result_short: dict[str, str | int | bool |
+                           list[str]] = self._check(password="A1!")
 
         self.assertFalse(result_empty["is_compliant"])
         self.assertFalse(result_short["is_compliant"])
 
-    def test_edge_case_whitespace_padding(self):
-        password_with_spaces = "MamaTata1                      "
-        user_inputs = prepare_user_inputs(
-            username="jkowalski", email="jan@wp.pl")
-        user_inputs.extend(GLOBAL_WORDS)
+    def test_edge_case_whitespace_padding(self) -> None:
+        """Password is short but filled with white spaces."""
+        password: str = "MamaTata1                      "
 
-        result = password_check(
-            password=password_with_spaces, user_inputs=user_inputs)
+        result: dict[str, str | int | bool | list[str]
+                     ] = self._check(password=password)
 
         self.assertFalse(result["is_compliant"])
 
-    def test_edge_case_case_insensitivity(self):
+    def test_edge_case_case_insensitivity(self) -> None:
+        """Check username inclusion."""
+        password: str = "jkowalskisecret2026"
+        username: str = "JKOWALSKI"
+        email: str = "jan.kowalski@securebank.pl"
 
-        username = "JKOWALSKI"
-        password = "jkowalskisecret2026"
-
-        user_inputs = prepare_user_inputs(
-            username=username, email="jan@securebank.pl")
-        user_inputs.extend(GLOBAL_WORDS)
-        result = password_check(password=password, user_inputs=user_inputs)
+        result: dict[str, str | int | bool | list[str]] = self._check(
+            password=password, username=username, email=email)
 
         self.assertFalse(result["is_compliant"])
+        self.assertIn(username.lower(), result["description"])
 
 
 if __name__ == "__main__":
